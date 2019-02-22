f58831e036afb3ac41f579aa0b9171a8eb9ab6e8
XuanYuan iMac <xyxdasnjss@163.com>
2019-02-22 19:25:53 +0800

    颜色渐变

---
+++ 颜色渐变/UIColor+UAExtension.h
@@ -0,0 +1,27 @@
+//
+//  UIColor+UAExtension.h
+//  UFoundation
+//
+//  Created by Think on 15/5/17.
+//  Copyright (c) 2015年 think. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (UAExtension)
+
+// Color gradual with progress from 0 -> 1.0
++ (UIColor *)colorGradualFrom:(UIColor *)startColor
+                          to:(UIColor *)endColor
+                    progress:(CGFloat)progress;
+
+// Get current rgba value
+- (CGFloat)redValue;
+- (CGFloat)greenValue;
+- (CGFloat)blueValue;
+- (CGFloat)alphaValue;
+
+// UIColor compare
+- (BOOL)isEqualToColor:(UIColor *)color;
+
+@end
---
+++ 颜色渐变/UIColor+UAExtension.m
@@ -0,0 +1,91 @@
+//
+//  UIColor+UAExtension.m
+//  UFoundation
+//
+//  Created by Think on 15/5/17.
+//  Copyright (c) 2015年 think. All rights reserved.
+//
+
+#import "UIColor+UAExtension.h"
+
+@implementation UIColor (UAExtension)
+
++ (UIColor *)colorGradualFrom:(UIColor *)startColor
+                          to:(UIColor *)endColor
+                    progress:(CGFloat)progress
+{
+    CGFloat sr = startColor.redValue * 255;
+    CGFloat sg = startColor.greenValue * 255;
+    CGFloat sb = startColor.blueValue * 255;
+    CGFloat sa = startColor.alphaValue;
+    
+    CGFloat er = endColor.redValue * 255;
+    CGFloat eg = endColor.greenValue * 255;
+    CGFloat eb = endColor.blueValue * 255;
+    CGFloat ea = endColor.alphaValue;
+    
+    CGFloat tr = sr + (er - sr) * progress;
+    CGFloat tg = sg + (eg - sg) * progress;
+    CGFloat tb = sb + (eb - sb) * progress;
+    CGFloat ta = sa + (ea - sa) * progress;
+    
+    return [UIColor colorWithRed:tr / 255. green:tg / 255. blue:tb / 255. alpha:ta];
+}
+
+- (CGFloat)redValue
+{
+    size_t count = CGColorGetNumberOfComponents(self.CGColor);
+    if (count != 4) {
+        return CGColorGetComponents(self.CGColor)[0];
+    }
+    
+    CGFloat value = CGColorGetComponents(self.CGColor)[0];
+    value = (value > 1.0)?1.0:value;
+    value = (value < 0.0)?0.0:value;
+    
+    return value;
+}
+
+- (CGFloat)greenValue
+{
+    size_t count = CGColorGetNumberOfComponents(self.CGColor);
+    if (count != 4) {
+        return CGColorGetComponents(self.CGColor)[0];
+    }
+    
+    CGFloat value = CGColorGetComponents(self.CGColor)[1];
+    value = (value > 1.0)?1.0:value;
+    value = (value < 0.0)?0.0:value;
+    
+    return value;
+}
+
+- (CGFloat)blueValue
+{
+    size_t count = CGColorGetNumberOfComponents(self.CGColor);
+    if (count != 4) {
+        return CGColorGetComponents(self.CGColor)[0];
+    }
+    
+    CGFloat value = CGColorGetComponents(self.CGColor)[2];
+    value = (value > 1.0)?1.0:value;
+    value = (value < 0.0)?0.0:value;
+    
+    return value;
+}
+
+- (CGFloat)alphaValue
+{
+    return CGColorGetAlpha(self.CGColor);
+}
+
+- (BOOL)isEqualToColor:(UIColor *)color
+{
+    if (color) {
+        return CGColorEqualToColor(self.CGColor, color.CGColor);
+    }
+    
+    return NO;
+}
+
+@end
